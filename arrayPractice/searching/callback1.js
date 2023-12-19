6335ecb7fee8ecf3e237b115c0bc1cb2838188f3
nitishkhobragade <138294846+nitishkhobragade@users.noreply.github.com>
2023-12-19 12:40:58 +0000

    callback using find

---
+++ arrayPractice/searching/callback1.js
@@ -0,0 +1,17 @@
+//using find function
+
+let courses = [
+  { no: 1, naam: "Nitish" },
+  { no: 2, naam: "Tejesh" },
+];
+console.log(courses);
+
+// console.log(courses.includes ( {no:1, naam: 'Nitish'} )); //gives us false even this is present
+
+//Now Using find method
+let course = courses.find(function (course) {
+  return course.naam == "Nitish";
+});
+
+console.log("using find method");
+console.log(course);
