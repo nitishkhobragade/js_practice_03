50c0e1889318b032f688dc3bacb781cafd2285e2
nitishkhobragade <138294846+nitishkhobragade@users.noreply.github.com>
2023-12-23 18:26:29 +0000

    update even odd

--- arrayPractice/odd_even/using_push.js
+++ arrayPractice/odd_even/using_push.js
@@ -6,33 +6,39 @@
 //display odd numbers
 //display even numbers
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+//Note without using any advance method do this with basic loop and conditional statement 
 
-let odd = 0;
-let even = 0;
+let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+let odd=0;
+let even=0;
 let oddArray = [];
-let evenArray = [];
+let evenArray =[];
 let evenSum = 0;
 let oddSum = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 == 0) {
+for(let i=0;i<numbers.length;i++){
+if(numbers[i] % 2==0){
     even++;
     evenArray.push(numbers[i]);
-    evenSum += numbers[i];
-  } else {
+    evenSum += numbers[i] ;
+
+
+}else{
     odd++;
     oddArray.push(numbers[i]);
     oddSum += numbers[i];
-  }
 }
-let result = even + odd;
 
-console.log("Even no :", even);
-console.log("Odd no :", odd);
-console.log(" Total Number of Even + Odd :", result);
-console.log("Even Array :", evenArray);
-console.log("odd Array :", oddArray);
-console.log("odd Sum :", oddSum);
-console.log("Even Sum  :", evenSum);
-console.log("Sum of All Numbers  :", evenSum + oddSum);
+}
+let result= even+odd;
+
+console.log("Total Even numbers :",even);
+console.log("Total Odd numbers :",odd);
+
+console.log(" Total Number of Even + Odd :",result);
+console.log("Even Numbers Are :",evenArray);
+console.log("Odd Numbers Are :",oddArray);
+console.log("Sum of Odd Numbers :",oddSum);
+console.log("Sum of Odd Numbers  :",evenSum);
+console.log("Sum of All Numbers  :",evenSum + oddSum);
