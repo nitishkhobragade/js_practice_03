bfe4b875307fec4743fc7bf74f5da3ec653aeb57
nitishkhobragade <138294846+nitishkhobragade@users.noreply.github.com>
2023-12-24 05:19:26 +0000

    2nd example forEach

---
+++ arrayPractice/forEach2.js
@@ -0,0 +1,11 @@
+/**
+ * @param {array} elements
+ */
+function loopThroughElements(elements) {
+  elements.forEach(function (element) {
+    console.log(element);
+  });
+}
+
+// Sample usage - do not modify
+loopThroughElements(["Sam", "Charlie", "Alex"]); // should log every name to the console
