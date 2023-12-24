70909333f7c7095c5cc22467d6a01212bbe905e8
nitishkhobragade <138294846+nitishkhobragade@users.noreply.github.com>
2023-12-24 18:13:07 +0000

    array_construction
    array_construction or declaration

---
+++ arrayPractice/declaration/array_constructor.js
@@ -0,0 +1,13 @@
+//declaring an array
+// Syntax:
+
+// let array = [value1, value2, ...];
+// let array = new Array();
+
+// Declaring an array using Array Constructor
+let language = new Array(3);
+language[0] = "HTML";
+language[1] = "CSS";
+language[2] = "Javascript";
+
+console.log(language);
---
+++ arrayPractice/declaration/literal_notation.js
@@ -0,0 +1,9 @@
+//declaring an array using literal notation
+// Syntax:
+
+// let array = [value1, value2, ...];
+// let array = new Array();
+
+//example
+let language = ["HTML", "CSS", "Javascript"];
+console.log(language);
