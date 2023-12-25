23c303d2c6d43121d7600808c7f762118529e7c1
nitishkhobragade <138294846+nitishkhobragade@users.noreply.github.com>
2023-12-25 14:10:22 +0000

    changing_array_elements

---
+++ arrayPractice/insertion/changing_element01.js
@@ -0,0 +1,6 @@
+//Change elements from a pre-defined array
+///Example : In the given example, we have changed the value of the first element that is ‘CSS’ to ‘DjNitish’
+const courses = ["HTML", "CSS", "Javascript"];
+console.log(courses);
+courses[1] = "DjNitish";
+console.log(courses);
