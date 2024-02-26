f34565cbc982e20fc735e825c34d3c8651667e3f
Nitish Kumar Khobragade <138294846+nitishkhobragade@users.noreply.github.com>
2024-02-26 15:03:42 +0000

    substr() method example 2 coded

--- javascript_objects/string_object/string_methods/substr()/example1.js
+++ javascript_objects/string_object/string_methods/substr()/example1.js
@@ -1,2 +1,4 @@
+////Let's see a simple example to print the part of the string.
+
 var str="Javatpoint";  
 document.write(str.substr(0,4));
---
+++ javascript_objects/string_object/string_methods/substr()/example2.js
@@ -0,0 +1,4 @@
+////Let's see one more example to print the part of the string.
+
+var str="Javatpoint";  
+document.write(str.substr(5,5));  
