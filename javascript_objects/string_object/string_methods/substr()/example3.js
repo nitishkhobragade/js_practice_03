f07e8149ff9baf5707dc92bdc14fc4cb4140d94e
Nitish Kumar Khobragade <138294846+nitishkhobragade@users.noreply.github.com>
2024-03-01 15:05:37 +0000

    substr() method fully coded
    substr() method fully coded  with 5 examples

---
+++ javascript_objects/string_object/string_methods/substr()/example3.js
@@ -0,0 +1,6 @@
+////In this example, we will provide only the starting index.
+
+var str = "JavaScript";
+document.write(str.substr(5));
+///2nd example
+document.write(str.substr(1));
---
+++ javascript_objects/string_object/string_methods/substr()/example5.js
@@ -0,0 +1,17 @@
+////In this example, we will provide negative number as a starting index. In such case, the method starts fetching from the end of the string.
+
+var str = "JavaScript";
+
+document.write(str.substr(-5, 5));
+
+// 2nd example
+document.write(str.substr(-5, 4));
+
+// 3rd example
+document.write(str.substr(-5, 3));
+
+// 4th example
+document.write(str.substr(-5, 2));
+
+// 5th example
+document.write(str.substr(-5, 1));
